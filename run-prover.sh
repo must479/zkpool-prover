5652c79eff18371f792e5910a6173d6828ca9c56
Must479 <67345599+must479@users.noreply.github.com>
2024-08-20 21:54:49 +0700

    Update run-prover.sh

--- run-prover.sh
+++ run-prover.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ACCESS_KEY=hfdiidOeiiishVVgKf3iPEeX69Y0y7Hr #replace the parameter with what we get in zkpool.io
 DEVICE_ID=1318455074 #replace the parameter with the id name you want to set
-POOL_ENDPOINT=http://taiko-a6-prover.zkpool.io/
+POOL_ENDPOINT=https://taiko-prover.zkpool.io
 
 if [ ! -f "./kzg_bn254_22.srs" ];then
     wget https://storage.googleapis.com/zkevm-circuits-keys/kzg_bn254_22.srs -P ./
